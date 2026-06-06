1d07cb7ec49ee77aabfa6bcd9e93b21ddf3f5ec6
Alexnitro777 <alexnitrohotz777@gmail.com>
2026-06-06 21:07:06 +0300

    update: embed+questions

--- src/questions.ts
+++ src/questions.ts
@@ -14,28 +14,44 @@ export interface Question {
 export const verifyQuestions: Question[] = [
   {
     id: 'source',
-    label: 'Откуда узнали про сервер?',
+    label: 'Откуда узнал о сервере?',
     style: TextInputStyle.Paragraph,
     required: true,
-    maxLength: 50,
+    maxLength: 200,
     placeholder:
       'Например: от конкретного участника, с конкретного сервера, из TikTok/Telegram по такой-то ссылке…',
   },
   {
     id: 'expectations',
-    label: 'Что ожидаете от сервера?',
+    label: 'Что ожидаешь от сервера?',
     style: TextInputStyle.Paragraph,
     required: true,
-    maxLength: 150,
-    placeholder: 'Например: хочу найти друзей, общение, компанию для игр, ивенты…',
+    maxLength: 200,
+    placeholder: 'Например: ищу друзей, тиммейтов, общение, компанию для игр…',
+  },
+  {
+    id: 'age',
+    label: 'Сколько вам лет?',
+    style: TextInputStyle.Short,
+    required: true,
+    maxLength: 4,
+    placeholder: 'Укажи свой реальный возраст',
+  },
+  {
+    id: 'community',
+    label: 'Твое отношение к фурри/фембой сообществу?',
+    style: TextInputStyle.Paragraph,
+    required: true,
+    maxLength: 300,
+    placeholder: 'Как относишься и относишь ли себя к нему — отвечай честно',
   },
   {
     id: 'rules',
-    label: 'Читали ли правила и согласны ли вы с ними?',
+    label: 'Правила прочитаны и приняты?',
     style: TextInputStyle.Short,
     required: true,
     maxLength: 30,
-    placeholder: 'Да, прочитал(а) и согласен(на)',
+    placeholder: 'Да',
   },
 ];
 
